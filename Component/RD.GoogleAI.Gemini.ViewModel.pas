338dc91ab86a81583122222ad4046fae8a7dc931
rd3 <ralph.dietrich@gmail.com>
2024-02-28 09:55:28 +0100

    input models

--- Component/RD.GoogleAI.Gemini.ViewModel.pas
+++ Component/RD.GoogleAI.Gemini.ViewModel.pas
@@ -2,6 +2,81 @@ unit RD.GoogleAI.Gemini.ViewModel;
 
 interface
 
+uses
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  REST.JSON.Types,
+  System.JSON,
+  System.Rtti,
+  System.TypInfo,
+  System.Character,
+  System.Math,
+  System.DateUtils,
+  IPPeerClient,
+  REST.Client,
+  REST.Authenticator.Basic,
+  Data.Bind.ObjectScope,
+  REST.Response.Adapter,
+  REST.Types,
+  REST.JSON,
+  RD.GoogleAI.Gemini.Model,
+  RD.GoogleAI.Input.Model,
+  System.Generics.Collections;
+{$METHODINFO ON}
+{$M+}
+
+type
+  RDGoogleAIException = class(Exception);
+
+  TRDGoogleAIConnection = class(TComponent)
+  public const
+    cVERSION = '1.0';
+    cDEF_URL = 'https://generativelanguage.googleapis.com/v1beta';
+    cDEF_MAX_OUTPUT_TOKENS = 400; // 100 means 60 to 80 words
+    cDEF_TEMP = 0.1;
+    cDEF_TOP_P = 0.95;
+    cDEF_TOP_K = 5;
+  private
+    FConfig: TGenerationConfig;
+    function GetVersion: String;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  published
+    property Config: TGenerationConfig read FConfig;
+  end;
+
+procedure Register;
+
 implementation
+
+procedure Register;
+begin
+  // RegisterComponents('RD AI', [TRD blabla]);
+end;
+
+{ TRDGoogleAIConnection }
+
+constructor TRDGoogleAIConnection.Create(AOwner: TComponent);
+begin
+  inherited;
+  FConfig := TGenerationConfig.Create;
+  FConfig.MaxOutputTokens := cDEF_MAX_OUTPUT_TOKENS;
+  FConfig.Temperature := cDEF_TEMP;
+  FConfig.TopK := cDEF_TOP_K;
+  FConfig.TopP := cDEF_TOP_P;
+end;
+
+destructor TRDGoogleAIConnection.Destroy;
+begin
+  FConfig.Free;
+  inherited;
+end;
+
+function TRDGoogleAIConnection.GetVersion: String;
+begin
+  Result := cVERSION;
+end;
 
 end.
---
+++ Component/RD.GoogleAI.Input.Model.pas
@@ -0,0 +1,166 @@
+unit RD.GoogleAI.Input.Model;
+
+interface
+
+uses
+  Pkg.Json.DTO,
+  System.Generics.Collections,
+  REST.Json.Types;
+
+{$M+}
+
+type
+  TParts = class;
+
+  TGenerationConfig = class(TJsonDTO)
+  private
+    FMaxOutputTokens: Integer;
+    [JSONName('stopSequences')]
+    FStopSequencesArray: TArray<string>;
+    [JSONMarshalled(False)]
+    FStopSequences: TList<string>;
+    FTemperature: Double;
+    FTopK: Integer;
+    FTopP: Double;
+    function GetStopSequences: TList<string>;
+  protected
+    function GetAsJson: string; override;
+  published
+    property MaxOutputTokens: Integer read FMaxOutputTokens write FMaxOutputTokens;
+    property StopSequences: TList<string> read GetStopSequences;
+    property Temperature: Double read FTemperature write FTemperature;
+    property TopK: Integer read FTopK write FTopK;
+    property TopP: Double read FTopP write FTopP;
+  public
+    destructor Destroy; override;
+  end;
+
+  TSafetySettings = class
+  private
+    FCategory: string;
+    FThreshold: string;
+  published
+    property Category: string read FCategory write FCategory;
+    property Threshold: string read FThreshold write FThreshold;
+  end;
+
+  TParts = class
+  private
+    FText: string;
+  published
+    property Text: string read FText write FText;
+  end;
+
+  TContents = class(TJsonDTO)
+  private
+    [JSONName('parts'), JSONMarshalled(False)]
+    FPartsArray: TArray<TParts>;
+    [GenericListReflect]
+    FParts: TObjectList<TParts>;
+    function GetParts: TObjectList<TParts>;
+  protected
+    function GetAsJson: string; override;
+  published
+    property Parts: TObjectList<TParts> read GetParts;
+  public
+    destructor Destroy; override;
+  end;
+
+  TRoot = class(TJsonDTO)
+  private
+    [JSONName('contents'), JSONMarshalled(False)]
+    FContentsArray: TArray<TContents>;
+    [GenericListReflect]
+    FContents: TObjectList<TContents>;
+    FGenerationConfig: TGenerationConfig;
+    [JSONName('safetySettings'), JSONMarshalled(False)]
+    FSafetySettingsArray: TArray<TSafetySettings>;
+    [GenericListReflect]
+    FSafetySettings: TObjectList<TSafetySettings>;
+    function GetContents: TObjectList<TContents>;
+    function GetSafetySettings: TObjectList<TSafetySettings>;
+  protected
+    function GetAsJson: string; override;
+  published
+    property Contents: TObjectList<TContents> read GetContents;
+    property GenerationConfig: TGenerationConfig read FGenerationConfig;
+    property SafetySettings: TObjectList<TSafetySettings> read GetSafetySettings;
+  public
+    constructor Create; override;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+{ TGenerationConfig }
+
+destructor TGenerationConfig.Destroy;
+begin
+  GetStopSequences.Free;
+  inherited;
+end;
+
+function TGenerationConfig.GetStopSequences: TList<string>;
+begin
+  Result := List<string>(FStopSequences, FStopSequencesArray);
+end;
+
+function TGenerationConfig.GetAsJson: string;
+begin
+  RefreshArray<string>(FStopSequences, FStopSequencesArray);
+  Result := inherited;
+end;
+
+{ TContents }
+
+destructor TContents.Destroy;
+begin
+  GetParts.Free;
+  inherited;
+end;
+
+function TContents.GetParts: TObjectList<TParts>;
+begin
+  Result := ObjectList<TParts>(FParts, FPartsArray);
+end;
+
+function TContents.GetAsJson: string;
+begin
+  RefreshArray<TParts>(FParts, FPartsArray);
+  Result := inherited;
+end;
+
+{ TRoot }
+
+constructor TRoot.Create;
+begin
+  inherited;
+  FGenerationConfig := TGenerationConfig.Create;
+end;
+
+destructor TRoot.Destroy;
+begin
+  FGenerationConfig.Free;
+  GetContents.Free;
+  GetSafetySettings.Free;
+  inherited;
+end;
+
+function TRoot.GetContents: TObjectList<TContents>;
+begin
+  Result := ObjectList<TContents>(FContents, FContentsArray);
+end;
+
+function TRoot.GetSafetySettings: TObjectList<TSafetySettings>;
+begin
+  Result := ObjectList<TSafetySettings>(FSafetySettings, FSafetySettingsArray);
+end;
+
+function TRoot.GetAsJson: string;
+begin
+  RefreshArray<TContents>(FContents, FContentsArray);
+  RefreshArray<TSafetySettings>(FSafetySettings, FSafetySettingsArray);
+  Result := inherited;
+end;
+
+end.
